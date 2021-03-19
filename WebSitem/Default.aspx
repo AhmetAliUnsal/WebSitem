3090a8e717bbd236a5f129c2ecbc8cb348f08bfa
Ahmet Ali UNSAL <ahmetaliunsal@posta.mu.edu.tr>
2021-03-19 20:46:56 +0300

    Fidan Satış Sitem

---
+++ WebSitem/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebSitem.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadAlan" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="SayfaBaslik" runat="server"> 
+    <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(template/img/bg-img/24.jpg);">
+            <h2>Anasayfa</h2>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#"><i class="fa fa-home"></i> Anasayfa</a></li>
+                            
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="OrtaAlan" runat="server">
+</asp:Content>
---
+++ WebSitem/Site.Master.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace WebSitem
+{
+
+
+    public partial class Site
+    {
+
+        /// <summary>
+        /// HeadAlan denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadAlan;
+
+        /// <summary>
+        /// SayfaBaslik denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder SayfaBaslik;
+
+        /// <summary>
+        /// OrtaAlan denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder OrtaAlan;
+    }
+}
