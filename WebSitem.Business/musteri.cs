3c3a5c7ec24c7316c5f38b55898dcb4147d4162e
Ahmet Ali UNSAL <ahmetaliunsal@posta.mu.edu.tr>
2021-04-10 12:54:16 +0300

    eklemeler

---
+++ WebSitem.Business/musteri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WebSitem.Business
+{
+    public class musteri
+    {
+
+        public List<WebSitem.DataAccess.musteri> Listele()
+        {
+            WebSitem.DataAccess.websayfaEntities ent= new DataAccess.websayfaEntities();
+            var sonuc = ent.musteri.ToList();
+            return sonuc;
+        }
+            
+
+    }
+}
---
+++ WebSitem/veri tabanı deneme.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebSitem
+{
+    public partial class veri_tabanı_deneme : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebSitem.Business.musteri veri = new Business.musteri();
+            var sonuc = veri.Listele();
+            GridView1.DataSource = sonuc;
+            GridView1.DataBind();
+        }
+    }
+}
